f8cba531e751a926ab7d6219b3cc9b7c86ae640a
Hides0nBush <nohope9876@gmail.com>
2025-02-13 02:52:36 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const titleElement = document.querySelector(".title");
+const buttonsContainer = document.querySelector(".buttons");
+const yesButton = document.querySelector(".btn--yes");
+const noButton = document.querySelector(".btn--no");
+const catImg = document.querySelector(".cat-img");
+
+const MAX_IMAGES = 10; // Supports up to 10 images
+
+let play = true;
+let noCount = 0;
+
+yesButton.addEventListener("click", handleYesClick);
+
+noButton.addEventListener("click", function () {
+  if (play) {
+    noCount++;
+    const imageIndex = Math.min(noCount, MAX_IMAGES);
+    changeImage(imageIndex);
+    resizeYesButton();
+    updateNoButtonText();
+    if (noCount === MAX_IMAGES) {
+      play = false;
+    }
+  }
+});
+
+function handleYesClick() {
+  titleElement.innerHTML = "Date on Saturday ; )"; // Updated message for "Yes"
+  buttonsContainer.classList.add("hidden");
+  changeImage("yes");
+}
+
+function resizeYesButton() {
+  const computedStyle = window.getComputedStyle(yesButton);
+  const fontSize = parseFloat(computedStyle.getPropertyValue("font-size"));
+  const newFontSize = fontSize * 1.6;
+
+  yesButton.style.fontSize = `${newFontSize}px`;
+}
+
+function generateMessage(noCount) {
+  const messages = [
+    "No", // 1
+    "Baby khit. dee dee la wa", // 2
+    "aw ;-;", // 3
+    "pls i khr hrng", // 4
+    "thar i liang baby Starbucks day", // 5
+    "how about flowers", // 6
+    "chocolate too?", // 7
+    "how about a date?", // 8
+    "How about all of the above", // 9
+    "phr lw br haiy click 'No' trng click yes theau ni", // 10
+  ];
+
+  const messageIndex = Math.min(noCount, messages.length - 1);
+  return messages[messageIndex];
+}
+
+function changeImage(image) {
+  catImg.src = `img/cat-${image}.jpg`;
+}
+
+function updateNoButtonText() {
+  noButton.innerHTML = generateMessage(noCount);
+}
